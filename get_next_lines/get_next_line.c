3c3c5b4e32715af67ff5d22491f7e111478cbbde
Maemoun Skhairi <maeskhai@c1r4p9.1337.ma>
2025-04-08 15:48:14 +0100

    so_long

---
+++ get_next_lines/get_next_line.c
@@ -0,0 +1,127 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maeskhai <maeskhai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/15 11:17:17 by maeskhai          #+#    #+#             */
+/*   Updated: 2025/01/28 13:48:03 by maeskhai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	ft_free(char **s1, char **s2)
+{
+	if (s1)
+	{
+		free(*s1);
+		*s1 = NULL;
+	}
+	if (s2)
+	{
+		free(*s2);
+		*s2 = NULL;
+	}
+}
+
+char	*ft_next(char *next1)
+{
+	char	*next2;
+	int		index;
+	int		i;
+
+	i = 0;
+	index = 0;
+	index = ft_strchr(next1, '\n');
+	if (index == -1)
+		return (NULL);
+	if (next1[index + 1] == 0)
+	{
+		ft_free(&next1, NULL);
+		return (NULL);
+	}
+	index++;
+	next2 = ft_calloc(ft_strlen(next1 + index) + 1, 1);
+	if (!next1)
+	{
+		ft_free(&next1, NULL);
+		return (NULL);
+	}
+	while (next1[index])
+		next2[i++] = next1[index++];
+	ft_free(&next1, NULL);
+	return (next2);
+}
+
+char	*ft_new_line(char *next1)
+{
+	char	*line;
+	int		index;
+
+	if (!next1 || !*next1)
+		return (NULL);
+	index = ft_strchr(next1, '\n');
+	if (index == -1)
+		return (next1);
+	else
+	{
+		index++;
+		line = ft_calloc(index + 1, 1);
+		if (!line)
+			return (NULL);
+	}
+	while (index--)
+		line[index] = next1[index];
+	return (line);
+}
+
+void	ft_readjoin(int fd, char **next1)
+{
+	char	*buff;
+	ssize_t	r;
+
+	r = 1;
+	buff = ft_calloc(BUFFER_SIZE + 1, 1);
+	if (!buff)
+	{
+		ft_free(next1, NULL);
+		return ;
+	}
+	while (ft_strchr(buff, '\n') == -1 && r != 0)
+	{
+		r = read(fd, buff, BUFFER_SIZE);
+		if (r == -1)
+		{
+			ft_free(next1, NULL);
+			break ;
+		}
+		buff[r] = '\0';
+		*next1 = ft_strjoin(*next1, buff);
+		if (!*next1)
+			break ;
+	}
+	ft_free(&buff, NULL);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*next1;
+
+	line = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	ft_readjoin(fd, &next1);
+	if (!next1)
+		return (NULL);
+	line = ft_new_line(next1);
+	if (!line)
+	{
+		ft_free(&next1, NULL);
+		return (NULL);
+	}
+	next1 = ft_next(next1);
+	return (line);
+}
---
+++ get_next_lines/get_next_line_utils.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maeskhai <maeskhai@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/15 11:17:20 by maeskhai          #+#    #+#             */
+/*   Updated: 2025/01/28 13:48:12 by maeskhai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	size_t	bytes;
+	size_t	i;
+	char	*p;
+
+	i = 0;
+	bytes = count * size;
+	if (size && bytes / size != count)
+		return (NULL);
+	p = malloc(bytes);
+	if (!p)
+		return (NULL);
+	while (i < bytes)
+	{
+		*(p + i) = 0;
+		i++;
+	}
+	return (p);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	int		join_len;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (!*s2)
+		return (s1);
+	join_len = ft_strlen(s1) + ft_strlen(s2);
+	str = ft_calloc((join_len + 1), 1);
+	if (!str)
+	{
+		ft_free(&s1, NULL);
+		return (NULL);
+	}
+	while (s1 && s1[i])
+		str[i++] = s1[j++];
+	ft_free(&s1, NULL);
+	j = 0;
+	while (s2[j])
+		str[i++] = s2[j++];
+	return (str);
+}
+
+int	ft_strchr(char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
